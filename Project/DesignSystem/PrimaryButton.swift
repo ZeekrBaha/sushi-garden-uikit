1a4f174b8678758acbe58bc76d96e0ab4fe3f570
zeekrbaha <baha.sadyr.i@gmail.com>
2026-05-20 22:45:43 -0500

    fix: add fatalError message in PrimaryButton coder init

--- Project/DesignSystem/PrimaryButton.swift
+++ Project/DesignSystem/PrimaryButton.swift
@@ -6,7 +6,7 @@ final class PrimaryButton: UIButton {
         configure()
     }
 
-    required init?(coder: NSCoder) { fatalError() }
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
     private func configure() {
         backgroundColor = AppColor.accent
